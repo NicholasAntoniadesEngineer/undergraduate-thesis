bc09c3aa1f59fecca10d586a85d052da1516a727
Nicholas Antoniades <31705980+NicholasAntoniadesEngineer@users.noreply.github.com>
2025-01-14 13:09:34 +0000

    Update stm32_lib.c

--- code/src/stm32_lib.c
+++ code/src/stm32_lib.c
@@ -43,17 +43,17 @@ void stm32_lib_port_init(port_config_t *port_config)
 // Initializing the PWM output
 void stm32_lib_init_pwm(int frequency)
 {
-	#define GPIO_AFRH_AFR11_AF2 ((uint32_t)0x00002000)		/*Macros to be defined*/
-	RCC->AHBENR |= RCC_AHBENR_GPIOBEN; 						// Enable clock for GPIOB
-	RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;	 					// Enable TIM2
-	GPIOB->MODER |= GPIO_MODER_MODER11_1;					// set PB10 to AF
-	GPIOB->AFR[1] |= (GPIO_AFRH_AFR11_AF2&(0b10<<12)); 		// Enable AF2 for PB10 in GPIOB AFR10
+	#define GPIO_AFRH_AFR11_AF2 ((uint32_t)0x00002000)	/*Macros to be defined*/
+	RCC->AHBENR |= RCC_AHBENR_GPIOBEN; 			// Enable clock for GPIOB
+	RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;	 		// Enable TIM2
+	GPIOB->MODER |= GPIO_MODER_MODER11_1;			// set PB10 to AF
+	GPIOB->AFR[1] |= (GPIO_AFRH_AFR11_AF2&(0b10<<12)); 	// Enable AF2 for PB10 in GPIOB AFR10
 	TIM2->CCMR2 |= (TIM_CCMR2_OC4M_2 | TIM_CCMR2_OC4M_1); 	// PWM Mode 1 	// PWM Mode 1
-	TIM2->ARR = frequency; 									// Setting signal frequency
-	TIM2->PSC = 0;											// Setting signal prescalar
-	TIM2->CCR4 = frequency/2; 								// PWM Duty cycle based on fractions of ARR
-	TIM2->CCER |= TIM_CCER_CC4E; 							// Compare 3 output enable
-	//TIM2->CR1 |= TIM_CR1_CEN;    							//Counter enable
+	TIM2->ARR = frequency; 					// Setting signal frequency
+	TIM2->PSC = 0;						// Setting signal prescalar
+	TIM2->CCR4 = frequency/2; 				// PWM Duty cycle based on fractions of ARR
+	TIM2->CCER |= TIM_CCER_CC4E; 				// Compare 3 output enable
+	//TIM2->CR1 |= TIM_CR1_CEN;    				//Counter enable
 }
 
 // Initializing the ADC
@@ -62,7 +62,7 @@ void stm32_lib_init_adc(void)
 	ADC1 -> CR |= ADC_CR_ADCAL;          // ADC calibration
 	RCC -> APB2ENR |= RCC_APB2ENR_ADCEN; // Enabling the ADC clock in the RCC APB  periperal clock enableregister
 	ADC1 -> CFGR1 |= 0x10;               // Setting the ADC resolution in the ADC configuration register 1
-	ADC1->CHSELR = 0b100000;	         // Selecting the chanel for pot-0.
+	ADC1->CHSELR = 0b100000;	     // Selecting the chanel for pot-0.
 	ADC1 -> CR |= ADC_CR_ADEN;           // Enabling the ADC
 	while((ADC1 -> ISR & 0x01) ==0 );    // Waiting for the ADRDY pin to let us know the ADC is ready.
 }
@@ -71,10 +71,10 @@ void stm32_lib_init_adc(void)
 int stm32_lib_adc_input(int input, int resolution)
 {
     ADC1->CFGR1 |= (resolution << 3);	/* configure resolution,12,10,8,6 bits */
-    ADC1->CHSELR = (1 <<  input);		// channel select where input ranges from ADC-in 0-13
-    ADC1->CR |= (0b1 << 2); 		    // start conversion
+    ADC1->CHSELR = (1 <<  input);	// channel select where input ranges from ADC-in 0-13
+    ADC1->CR |= (0b1 << 2); 		// start conversion
     while ((ADC1->ISR & 0b100) == 0);	// wait for conversion to end
-    return ADC1->DR;					// return value
+    return ADC1->DR;			// return value
 }
 
 int stm32_lib_adc_awd_check(void)
@@ -93,12 +93,12 @@ void stm32_lib_adc_awd_8bit(int ADC_channel, int ADC_Low_threshhold, int ADC_Hig
 	// ADC_Low_threshhold = (ADC_Low_threshhold*256)/3.3;
 	// ADC_High_threshhold = (ADC_High_threshhold*256)/3.3;
 
-	ADC1 -> CFGR1 |= 1 << 22;  					// Set AWDEN bit high to enable AWD
-	ADC1 -> CFGR1 |= 1 << 21;					// Set AWDSGL bit high to select signle channel.
-	ADC1 -> CFGR1 |= ADC_channel << 25;			// Set AWDCH[4:0] bits to select channel.
+	ADC1 -> CFGR1 |= 1 << 22;  			// Set AWDEN bit high to enable AWD
+	ADC1 -> CFGR1 |= 1 << 21;			// Set AWDSGL bit high to select signle channel.
+	ADC1 -> CFGR1 |= ADC_channel << 25;		// Set AWDCH[4:0] bits to select channel.
 	ADC1 -> TR |= ADC_High_threshhold << 15;	// Set ADC_HTR high threshholds.
-	ADC1 -> TR |= ADC_Low_threshhold;			// Set ADC_LTR low threshhold.
-	ADC1 -> IER |= 0x80;						// Set AWDIE bit in ADC_IER to enable the interrupt.
+	ADC1 -> TR |= ADC_Low_threshhold;		// Set ADC_LTR low threshhold.
+	ADC1 -> IER |= 0x80;				// Set AWDIE bit in ADC_IER to enable the interrupt.
 }
 
 // Fetching ADC data
@@ -161,11 +161,11 @@ void stm32_lib_ack_irq(void)
 // Initializing the External Interrupt
 void stm32_lib_init_exti(void)
 {
-	RCC->APB2ENR |= RCC_APB2ENR_SYSCFGCOMPEN;		// Enables the clock for the sys config controller.
+	RCC->APB2ENR |= RCC_APB2ENR_SYSCFGCOMPEN;	// Enables the clock for the sys config controller.
 	SYSCFG->EXTICR[1] |=SYSCFG_EXTICR1_EXTI1_PA;    // Map PA1 to EXTI1.
-	EXTI->IMR |=EXTI_IMR_MR1; 						// Unmasks EXTI1 in interrupt mask register
-	EXTI->FTSR |= EXTI_FTSR_TR1; 					// Falling trigger enable for input line 1 (SW1)
-	NVIC_EnableIRQ(EXTI0_1_IRQn);					// Enable the interrupt in the NVIC
+	EXTI->IMR |=EXTI_IMR_MR1; 			// Unmasks EXTI1 in interrupt mask register
+	EXTI->FTSR |= EXTI_FTSR_TR1; 			// Falling trigger enable for input line 1 (SW1)
+	NVIC_EnableIRQ(EXTI0_1_IRQn);			/ Enable the interrupt in the NVIC
 }
 
 
@@ -174,54 +174,54 @@ void stm32_lib_init_usart1(void)
 	// Must enable usart1_DE and the associated pins AF
 	// Program the M bits in USARTx_CR1 to define the word length.
 	// Program the number of stop bits in USARTx_CR2.
-	RCC->APB2ENR |= RCC_APB2ENR_USART1EN; 				// enable clock for UART1
-	RCC->AHBENR |= RCC_AHBENR_GPIOAEN; 					// enable clock to port a
-	GPIOA->MODER |= GPIO_MODER_MODER9_1; 				// Set PA9 to AF mode
-	GPIOA->MODER |= GPIO_MODER_MODER10_1;				// Set PA10 to AF mode
-	GPIOA->AFR[1] = 0b00000000000000000000000100010000; // Map AF1 for PA9 and PA10
-	USART1->CR1 &=~ USART_CR1_M; 						// clear M0(bit 12) of USARTx_CR1
-	SystemCoreClockUpdate(); 							// define clock speed
+	RCC->APB2ENR |= RCC_APB2ENR_USART1EN; 			// enable clock for UART1
+	RCC->AHBENR |= RCC_AHBENR_GPIOAEN; 			// enable clock to port a
+	GPIOA->MODER |= GPIO_MODER_MODER9_1; 			// Set PA9 to AF mode
+	GPIOA->MODER |= GPIO_MODER_MODER10_1;			// Set PA10 to AF mode
+	GPIOA->AFR[1] = 0b00000000000000000000000100010000; 	// Map AF1 for PA9 and PA10
+	USART1->CR1 &=~ USART_CR1_M; 				// clear M0(bit 12) of USARTx_CR1
+	SystemCoreClockUpdate(); 				// define clock speed
 	USART1->BRR = SystemCoreClock/115200;		     	// set baud rate to 115.2kbps for oversampling by 16
-	USART1->CR1 &=~ USART_CR1_PCE;						// no parity
-	USART1->CR2 &=~ USART_CR2_STOP;						// 1 stop bit
-	USART1->CR1 |= USART_CR1_UE;						// Enable USART1
-	USART1->CR1 |= USART_CR1_TE;						// Enable USART1_TE Set idle frame as first transmission.
-	USART1->CR1 |= USART_CR1_RE;						// Enable USART1_RX
+	USART1->CR1 &=~ USART_CR1_PCE;				// no parity
+	USART1->CR2 &=~ USART_CR2_STOP;				// 1 stop bit
+	USART1->CR1 |= USART_CR1_UE;				// Enable USART1
+	USART1->CR1 |= USART_CR1_TE;				// Enable USART1_TE Set idle frame as first transmission.
+	USART1->CR1 |= USART_CR1_RE;				// Enable USART1_RX
 }
 
 void stm32_lib_usart1_transmit(unsigned char DataToTx)
 {
 	// wait until TXE = 1 before writing int tdr
-	USART1 -> TDR = DataToTx; 					// write character �c� to the USART1_TDR
-	while((USART1 -> ISR & USART_ISR_TC) == 0); // wait: transmission complete
+	USART1 -> TDR = DataToTx; 			// write character �c� to the USART1_TDR
+	while((USART1 -> ISR & USART_ISR_TC) == 0); 	// wait: transmission complete
 }
 
 unsigned char stm32_lib_usart1_receive(void)
 {
-	unsigned char DataRx = 'z';					  // global
-	while((USART1 -> ISR & USART_ISR_RXNE) == 0); // exits loop when data is received
-	DataRx = USART1 -> RDR; 					  // store received data into �DataRx� variable
+	unsigned char DataRx = 'z';			// global
+	while((USART1 -> ISR & USART_ISR_RXNE) == 0); 	// exits loop when data is received
+	DataRx = USART1 -> RDR; 			// store received data into �DataRx� variable
 	USART1 -> ICR = 0b111111111111111111111;
 	return DataRx;
 }
 
 void stm32_lib_lock_crystal(void) 
 {
-  RCC->CR |= RCC_CR_HSEON;								// enable HSE
-  while(!(RCC->CR & RCC_CR_HSERDY)); 					// hang here until HSE ready
+  RCC->CR |= RCC_CR_HSEON;				// enable HSE
+  while(!(RCC->CR & RCC_CR_HSERDY)); 			// hang here until HSE ready
   // the following adds a wait state to Flash reads and enables the prefetch buffer. This may or may not be necessary...
   FLASH->ACR = FLASH_ACR_PRFTBE | FLASH_ACR_LATENCY;
-  RCC->CFGR |= RCC_CFGR_PLLMUL6; 						// PLLCLK = HSE * 6 = 8 * 6 = 48 MHz = maximum
-  RCC->CFGR |= RCC_CFGR_PLLSRC_HSE_PREDIV; 				// select HSE as source for PLL
-  RCC->CR |= RCC_CR_PLLON; 								// enable the PLL
-  while(!(RCC->CR & RCC_CR_PLLRDY)); 					// hang here until PLL ready
-  RCC->CFGR |= RCC_CFGR_SW_PLL;							// SYSCLK sourced from PLL
-  while(!(RCC->CFGR & RCC_CFGR_SWS_PLL)); 				// hang until SYSLK switched
+  RCC->CFGR |= RCC_CFGR_PLLMUL6; 			// PLLCLK = HSE * 6 = 8 * 6 = 48 MHz = maximum
+  RCC->CFGR |= RCC_CFGR_PLLSRC_HSE_PREDIV; 		// select HSE as source for PLL
+  RCC->CR |= RCC_CR_PLLON; 				// enable the PLL
+  while(!(RCC->CR & RCC_CR_PLLRDY)); 			// hang here until PLL ready
+  RCC->CFGR |= RCC_CFGR_SW_PLL;				// SYSCLK sourced from PLL
+  while(!(RCC->CFGR & RCC_CFGR_SWS_PLL)); 		// hang until SYSLK switched
 }
 
 void stm32_lib_unlock_crystal(void) 
 {
-  RCC->CFGR &= ~RCC_CFGR_SW; 			// clear the SYSCLK selection bits, causing SYSCLK to be sourced from HSI
+  RCC->CFGR &= ~RCC_CFGR_SW; 		// clear the SYSCLK selection bits, causing SYSCLK to be sourced from HSI
   while(RCC->CFGR & RCC_CFGR_SWS_PLL); 	// hang until SYSLK no longer PLL
   RCC->CR &= ~RCC_CR_HSEON;
 }
@@ -296,7 +296,7 @@ void stm32_lib_pwm(void)
     TIM2->PSC = 0;
 
     //PWM Duty cycle based on fractions of ARR
-    TIM2->CCR3 = 0 * 480; 	// Red = 20%
+    TIM2->CCR3 = 0 * 480;   // Red = 20%
     TIM2->CCR4 = 20 * 480;  // Green = 90%
 
     // Enable output compare for CH3 and CH4
